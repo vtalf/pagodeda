2e887c82b56227d0358199cbfc87514f8c837b05
IgorDuca <igorducca@gmail.com>
2020-08-04 01:31:52 -0300

    :sunny: Innitial Commit

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
+const api = express();
+api.use(express.static(__dirname + '/public'))
+api.use(bodyParser.json())
+
+const port = process.env.PORT || 5555
+
+api.listen(port, () => {
+    console.log(`Pagodeda!? Ouvindo na porta: ${port}`)
+    console.log(`http://localhost:${port}`)
+})
